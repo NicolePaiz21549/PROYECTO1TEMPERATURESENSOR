43a9ba3f5554038250d0f1494bcd1fd1e3f09ab3
NicolePaiz21549 <84263290+NicolePaiz21549@users.noreply.github.com>
2023-08-21 23:09:31 -0600

    Update main.cpp

--- TEMPERATURESENSORPRO/src/main.cpp
+++ TEMPERATURESENSORPRO/src/main.cpp
@@ -8,34 +8,17 @@ el promedio, lo que permite reducir el ruido y así las lecturas de temperatura 
 #define BUTTON_PIN 13
 int LM35_Input = 0;
 float TempC = 0.0;
-//float TempF = 0.0;
+float TempF = 0.0;
 float Voltage = 0.0;
 bool buttonPressed = false;
-
-// Variables para promediar las lecturas de temperaturas obtenidas
-//const int numReadings = 10; // Número de lecturas para promediar
-//int readings[numReadings];  // Array para almacenar las lecturas de temperaturas
-//int readIndex = 0;          // índice para la leer las lecturas de temperaturas en el array
-//long total = 0;             // Total acumulado de lecturas
-
-// Calibración ADC para ESP32
-esp_adc_cal_characteristics_t adc_chars;
 
 // Prototipo de función para readADC_Cal
 uint32_t readADC_Cal(int ADC_Raw);
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-
-  // Inicialización de las lecturas del array
-  //for (int i = 0; i < numReadings; i++) {
-    //readings[i] = 0;
-  //}
-
-  // Inicialización de la calibración ADC para ESP32
-  esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);
 }
 
 void loop()
@@ -45,20 +28,14 @@ void loop()
     if (!buttonPressed)
     {
       buttonPressed = true;
-      
-      // Lecturas promedio del ADC
-      //total = total - readings[readIndex];
-      //readings[readIndex] = analogRead(LM35_GPIO_PIN);
-      //total = total + readings[readIndex];
-      //readIndex = (readIndex + 1) % numReadings;
-      //LM35_Input = total / numReadings;
-
+      LM35_Input = analogRead(LM35_GPIO_PIN);
       Voltage = readADC_Cal(LM35_Input);
-      TempC = ((Voltage/4095)*3.3)/0.01; //Conversión para dar la temperatura en ℃
-      //TempC = Voltage/10;
-
-      Serial.print("Temperatura en °C = ");
-      Serial.print(TempC, 1);
+      TempC = Voltage / 10;
+      TempF = (TempC * 1.8) + 32;
+      Serial.print("Temperature in °C = ");
+      Serial.print(TempC);
+      Serial.print(",  Temperature in °F = ");
+      Serial.println(TempF);
     }
   }
   else
@@ -70,6 +47,7 @@ void loop()
 // Definición de la función readADC_Cal
 uint32_t readADC_Cal(int ADC_Raw)
 {
-  //Conversión directa del valor ADC en voltaje utilizando la calibración ADC del ESP32 
-  return esp_adc_cal_raw_to_voltage(ADC_Raw, &adc_chars);
+  esp_adc_cal_characteristics_t adc_chars;
+  esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);
+  return(esp_adc_cal_raw_to_voltage(ADC_Raw, &adc_chars));
 }
