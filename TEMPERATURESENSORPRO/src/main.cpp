480e6cc8e260d63d12e2b574bd0080b6d66b7338
NicolePaiz21549 <84263290+NicolePaiz21549@users.noreply.github.com>
2023-08-21 22:47:27 -0600

    Update main.cpp

--- TEMPERATURESENSORPRO/src/main.cpp
+++ TEMPERATURESENSORPRO/src/main.cpp
@@ -1,6 +1,3 @@
-//Parte 1 - Sensor de Temperatura
-/*Se implementó un promedio mediante el uso de un array para almacenar múltiples lecturas de ADC calculando así
-el promedio, lo que permite reducir el ruido y así las lecturas de temperatura son más uniformes*/
 #include <Arduino.h>
 #include <esp_adc_cal.h>
 
@@ -8,45 +5,50 @@ el promedio, lo que permite reducir el ruido y así las lecturas de temperatura 
 #define BUTTON_PIN 13
 int LM35_Input = 0;
 float TempC = 0.0;
-//float TempF = 0.0;
 float Voltage = 0.0;
 bool buttonPressed = false;
 
-// Variables para promediar las lecturas de temperaturas obtenidas
-const int numReadings = 10; // Número de lecturas para promediar
-int readings[numReadings];  // Array para almacenar las lecturas de temperaturas
-int readIndex = 0;          // índice para la leer las lecturas de temperaturas en el array
-long total = 0;             // Total acumulado de lecturas
+// Variables for averaging temperature readings
+const int numReadings = 10;
+int readings[numReadings];
+int readIndex = 0;
+long total = 0;
 
-// Calibración ADC para ESP32
+// Calibration ADC for ESP32
 esp_adc_cal_characteristics_t adc_chars;
 
-// Prototipo de función para readADC_Cal
+// Prototyping readADC_Cal function
 uint32_t readADC_Cal(int ADC_Raw);
+
+// Refresh interval and temperature change threshold
+const unsigned long refreshInterval = 5000; // Refresh every 5 seconds
+const float tempChangeThreshold = 0.2; // Temperature change threshold in °C
+unsigned long lastRefreshTime = 0;
+float lastTempC = 0.0;
 
 void setup()
 {
   Serial.begin(115200);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
 
-  // Inicialización de las lecturas del array
   for (int i = 0; i < numReadings; i++) {
     readings[i] = 0;
   }
 
-  // Inicialización de la calibración ADC para ESP32
   esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);
 }
 
 void loop()
 {
+  unsigned long currentTime = millis();
+
   if (digitalRead(BUTTON_PIN) == LOW)
   {
-    if (!buttonPressed)
+    if (!buttonPressed && currentTime - lastRefreshTime >= refreshInterval)
     {
       buttonPressed = true;
-      
-      // Lecturas promedio del ADC
+      lastRefreshTime = currentTime;
+
       total = total - readings[readIndex];
       readings[readIndex] = analogRead(LM35_GPIO_PIN);
       total = total + readings[readIndex];
@@ -54,11 +56,14 @@ void loop()
       LM35_Input = total / numReadings;
 
       Voltage = readADC_Cal(LM35_Input);
-      TempC = ((Voltage/4095)*3.3)/0.01; //Conversión para dar la temperatura en ℃
-      //TempC = Voltage/10;
+      TempC = ((Voltage/4095)*3.3)/0.01;
 
-      Serial.print("Temperatura en °C = ");
-      Serial.print(TempC, 1);
+      // Check if temperature has changed significantly
+      if (abs(TempC - lastTempC) >= tempChangeThreshold) {
+        lastTempC = TempC;
+        Serial.print("Temperatura en °C = ");
+        Serial.print(TempC, 1);
+      }
     }
   }
   else
@@ -67,11 +72,7 @@ void loop()
   }
 }
 
-// Definición de la función readADC_Cal
 uint32_t readADC_Cal(int ADC_Raw)
 {
-  //Conversión directa del valor ADC en voltaje utilizando la calibración ADC del ESP32 
   return esp_adc_cal_raw_to_voltage(ADC_Raw, &adc_chars);
 }
-
-//Probar agregar un tiempo más para tomar temperatura corporal
