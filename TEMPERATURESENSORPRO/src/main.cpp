9bc72e9d422221b3bf3b40c96dabedc7851f9098
NicolePaiz21549 <84263290+NicolePaiz21549@users.noreply.github.com>
2023-08-28 09:48:39 -0600

    Update main.cpp

--- TEMPERATURESENSORPRO/src/main.cpp
+++ TEMPERATURESENSORPRO/src/main.cpp
@@ -31,12 +31,14 @@ void loop() {
       buttonPressed = true;
 
       // Lectura del valor LM35_ADC
-      TempC=((analogRead(LM35_GPIO_PIN)+70)*(5000/4096)); //Sumatoria de un valor de OFFSET para el valor análogo original multiplicado por el voltaje de 5V por pin Vin para la fórmula dividido la resolución ADC de 12bits
+      TempC=((analogRead(LM35_GPIO_PIN)+70)*(5000.0/4096.0)); //Sumatoria de un valor de OFFSET para el valor análogo original multiplicado por el voltaje de 5V por pin Vin para la fórmula dividido la resolución ADC de 12bits
       //+ grados celsius de OFFSET para obtener un valor más precisos
-      TempC=TempC/10; //División dentro de 10 representando los 10mV del LM35 ya que cada cambio de 10mV representa un cambio de 1℃
+      TempC=TempC/10.0; //División dentro de 10 representando los 10mV del LM35 ya que cada cambio de 10mV representa un cambio de 1℃
 
       Serial.print("Temperatura en °C = ");
       Serial.print(TempC);
+      Serial.print(",  Lectura analógica pura = ");
+      Serial.println(analogRead(LM35_GPIO_PIN));
       Serial.println();
     }
   } else {
